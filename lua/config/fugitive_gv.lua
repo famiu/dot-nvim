0b76945eb7864209ec270e153401fbe3ac3ddc3a
Famiu Haque <famiuhaque@gmail.com>
2021-03-29 21:07:12 +0600

    Replace deprecated fugitive functions

--- lua/config/fugitive_gv.lua
+++ lua/config/fugitive_gv.lua
@@ -2,10 +2,10 @@ local bind = vim.api.nvim_set_keymap
 
 bind('n', '<Space>gb', ':Gblame<CR>', {})
 bind('n', '<Space>gs', ':Gstatus<CR>', {})
-bind('n', '<Space>gc', ':Gcommit -v<CR>', {})
+bind('n', '<Space>gc', ':Git commit -v<CR>', {})
 bind('n', '<Space>ga', ':Git add -p<CR>', {})
-bind('n', '<Space>gm', ':Gcommit --amend<CR>', {})
-bind('n', '<Space>gp', ':Gpush<CR>', {})
+bind('n', '<Space>gm', ':Git commit --amend<CR>', {})
+bind('n', '<Space>gp', ':Git push<CR>', {})
 bind('n', '<Space>gd', ':Gdiff<CR>', {})
 bind('n', '<Space>gw', ':Gwrite<CR>', {})
 bind('n', '<Space>gv', ':GV<CR>', {})
