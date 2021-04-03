426e9bdf9ef5d950fb4783b519f6c368507e4b77
Famiu Haque <famiuhaque@gmail.com>
2021-04-03 21:53:46 +0600

    Startify show icons

--- lua/config/ui/startify.lua
+++ lua/config/ui/startify.lua
@@ -1,1 +1,14 @@
 vim.g.startify_custom_header = 'startify#pad(startify#fortune#cowsay())'
+
+-- Use Nvim-web-devicons for Startify
+function _G.webDevIcons(path)
+    local filename = vim.fn.fnamemodify(path, ':t')
+    local extension = vim.fn.fnamemodify(path, ':e')
+    return require'nvim-web-devicons'.get_icon(filename, extension, { default = true })
+end
+
+vim.api.nvim_exec([[
+function! StartifyEntryFormat() abort
+    return 'v:lua.webDevIcons(absolute_path) . " " . entry_path'
+endfunction
+]], false)
