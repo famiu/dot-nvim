d5c16d85ea3cbbe17a30727d76a30861b94b48ef
Famiu Haque <famiuhaque@gmail.com>
2021-04-03 17:07:25 +0600

    Update godot configuration to prevent keybind conflicts

--- lua/config/langs/godot.lua
+++ lua/config/langs/godot.lua
@@ -2,10 +2,19 @@ local bind = vim.api.nvim_buf_set_keymap
 local utils = require('utils')
 
 function GodotSettings()
-	bind(0, 'n', '<F4>', ':GodotRunLast<CR>', { noremap = true })
-	bind(0, 'n', '<F5>', ':GodotRun<CR>', { noremap = true })
-	bind(0, 'n', '<F6>', ':GodotRunCurrent<CR>', { noremap = true })
-	bind(0, 'n', '<F7>', ':GodotRunFZF<CR>', { noremap = true })
+    bind(0, 'n', '<LocalLeader>l', ':GodotRunLast<CR>', { noremap = true })
+    bind(0, 'n', '<LocalLeader>r', ':GodotRun<CR>', { noremap = true })
+    bind(0, 'n', '<LocalLeader>c', ':GodotRunCurrent<CR>', { noremap = true })
+    bind(0, 'n', '<LocalLeader>f', ':GodotRunFZF<CR>', { noremap = true })
+
+    local keys = {
+        l = 'Run last',
+        r = 'Run',
+        c = 'Run Current',
+        f = 'Run FZF'
+    }
+
+    require('whichkey_setup').register_keymap('localleader', keys)
 end
 
 utils.create_augroup({
