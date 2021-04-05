c43f4ac65b09e9e0cc65317de62895628f31d491
Famiu Haque <famiuhaque@gmail.com>
2021-04-05 11:35:07 +0600

    Load utility after tools

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,7 +1,7 @@
 -- Configure plugins and features that depend on plugins
 require('config.ui')
-require('config.utility')
 require('config.tools')
+require('config.utility')
 require('config.langs')
 require('config.misc')
 require('config.lsp')
