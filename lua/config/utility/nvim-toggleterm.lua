36d3ebc47b91ff8a2e58c13ae115d8138b483f22
Famiu Haque <famiuhaque@gmail.com>
2021-04-05 20:33:14 +0600

    Update toggleterm config

--- lua/config/utility/nvim-toggleterm.lua
+++ lua/config/utility/nvim-toggleterm.lua
@@ -1,5 +1,5 @@
 require"toggleterm".setup {
-    size = 20,
+    size = 14,
     open_mapping = [[<A-=>]],
     shade_filetypes = {},
     shade_terminals = true,
@@ -7,6 +7,6 @@ require"toggleterm".setup {
     -- default: 1 for dark backgrounds, 3 for light
     shading_factor = 1,
     start_in_insert = true,
-    persist_size = false,
+    persist_size = true,
     direction = 'horizontal',
 }
