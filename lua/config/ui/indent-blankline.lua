dafd4ed8e77e27ec1c2f2f3fc32be4e9234fefa4
Famiu Haque <famiuhaque@gmail.com>
2021-04-09 19:01:58 +0600

    Add indent guides

---
+++ lua/config/ui/indent-blankline.lua
@@ -0,0 +1,17 @@
+local g = vim.g
+
+g.indent_blankline_enabled = true
+
+g.indent_blankline_char = '│'
+g.indent_blankline_space_char_blankline = ' '
+
+g.indent_blankline_indent_level = 10
+g.indent_blankline_show_first_indent_level = true
+g.indent_blankline_show_trailing_blankline_indent = false
+
+g.indent_blankline_use_treesitter = true
+g.indent_blankline_show_current_context = true
+vim.g.indent_blankline_context_patterns = {'class', 'function', 'method', '^if', '^while', '^for', '^object', '^table', 'block', 'arguments'}
+
+g.indent_blankline_filetype_exclude = {'help', 'startify', 'NvimTree', 'undotree'}
+g.indent_blankline_buftype_exclude = {'terminal', 'nofile'}
