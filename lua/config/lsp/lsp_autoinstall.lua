2738f8956d65ce2bc892c5b37934664c2229f71f
Famiu Haque <famiuhaque@gmail.com>
2021-04-09 12:40:12 +0600

    Add autoinstall support

--- lua/config/lsp/lsp_autoinstall.lua
+++
@@ -1,8 +0,0 @@
--- Install missing servers
-local required_servers = { "lua", "python" }
-local installed_servers = require'lspinstall'.installed_servers()
-for _, server in pairs(required_servers) do
-  if not vim.tbl_contains(installed_servers, server) then
-    require'lspinstall'.install_server(server)
-  end
-end
