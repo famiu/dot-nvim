0281e8416a67bc092eeaa5950cec2826938acb55
Famiu Haque <famiuhaque@gmail.com>
2021-04-10 12:11:15 +0600

    Improve whichkey config

--- lua/config/utility/whichkey.lua
+++ lua/config/utility/whichkey.lua
@@ -1,3 +1,5 @@
+local g = vim.g
+
 require("whichkey_setup").config{
     hide_statusline = true,
     default_keymap_settings = {
@@ -5,3 +7,17 @@ require("whichkey_setup").config{
         noremap=true,
     },
 }
+
+g.which_key_position = 'botright'
+g.which_key_sep = '→'
+g.which_key_hspace = 5
+g.which_key_max_size = 0
+g.which_key_vertical = false
+g.which_key_flatten = true
+g.which_key_centered = true
+g.which_key_use_floating_win = false
+g.which_key_sort_horizontal = false
+g.which_key_align_by_seperator = true
+g.which_key_run_map_on_popup = true
+g.which_key_ignore_invalid_key = false
+g.which_key_fallback_to_native_key = true
