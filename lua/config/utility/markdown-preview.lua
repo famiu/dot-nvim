21ca8cfcc7d53f6eeac6c5220769b2f4a8e53a53
Famiu Haque <famiuhaque@gmail.com>
2021-04-13 17:10:01 +0600

    Don't auto close markdown

--- lua/config/utility/markdown-preview.lua
+++ lua/config/utility/markdown-preview.lua
@@ -2,7 +2,7 @@ local g = vim.g
 local bind = vim.api.nvim_buf_set_keymap
 
 g.mkdp_auto_start = 0
-g.mkdp_auto_close = 1
+g.mkdp_auto_close = 0
 g.mkdp_refresh_slow = 0
 g.mkdp_command_for_global = 0
 g.mkdp_open_to_the_world = 0
