6a7b41eadcef5e49d9b8af7515940f948c78d19d
Famiu Haque <famiuhaque@gmail.com>
2021-04-14 22:08:42 +0600

    Add soft navigation to markdown

--- lua/config/langs/markdown.lua
+++ lua/config/langs/markdown.lua
@@ -2,6 +2,8 @@
 local bind = vim.api.nvim_buf_set_keymap
 
 function MarkdownSettings()
+    require('utils').set_buffer_soft_line_nagivation()
+
     bind(0, 'n', '<LocalLeader>p', '<Plug>MarkdownPreview', {})
     bind(0, 'n', '<LocalLeader>s', '<Plug>MarkdownPreviewStop', {})
     bind(0, 'n', '<LocalLeader>t', '<Plug>MarkdownPreviewToggle', {})
--- lua/utils.lua
+++ lua/utils.lua
@@ -1,6 +1,7 @@
 local M = {}
 local cmd = vim.cmd
 local types = {o = vim.o, b = vim.bo, w = vim.wo}
+local buf_bind = vim.api.nvim_buf_set_keymap
 
 -- Get table length
 function M.length(table)
@@ -93,6 +94,38 @@ function M.create_augroup(autocmds, name)
     end
 
     cmd('augroup END')
+end
+
+-- Make navigation keys navigate through display lines instead of physical lines
+function M.set_buffer_soft_line_nagivation()
+    local opts = { noremap = true, silent = true }
+
+    buf_bind(0, 'n', 'k', 'gk', opts)
+    buf_bind(0, 'n', 'j', 'gj', opts)
+    buf_bind(0, 'n', '0', 'g0', opts)
+    buf_bind(0, 'n', '^', 'g^', opts)
+    buf_bind(0, 'n', '$', 'g$', opts)
+
+    buf_bind(0, 'n', '<Up>', 'gk', opts)
+    buf_bind(0, 'n', '<Down>', 'gj', opts)
+    buf_bind(0, 'n', '<Home>', 'g<Home>', opts)
+    buf_bind(0, 'n', '<End>', 'g<End>', opts)
+
+    buf_bind(0, 'o', 'k', 'gk', opts)
+    buf_bind(0, 'o', 'j', 'gj', opts)
+    buf_bind(0, 'o', '0', 'g0', opts)
+    buf_bind(0, 'o', '^', 'g^', opts)
+    buf_bind(0, 'o', '$', 'g$', opts)
+
+    buf_bind(0, 'o', '<Up>', 'gk', opts)
+    buf_bind(0, 'o', '<Down>', 'gj', opts)
+    buf_bind(0, 'o', '<Home>', 'g<Home>', opts)
+    buf_bind(0, 'o', '<End>', 'g<End>', opts)
+
+    buf_bind(0, 'i', '<Up>', '<C-o>gk', opts)
+    buf_bind(0, 'i', '<Down>', '<C-o>gj', opts)
+    buf_bind(0, 'i', '<Home>', '<C-o>g<Home>', opts)
+    buf_bind(0, 'i', '<End>', '<C-o>g<End>', opts)
 end
 
 return M
