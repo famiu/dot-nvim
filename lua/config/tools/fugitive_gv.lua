e77f29e6233b35ac2f14bc2c62be0eee691a02f7
Famiu Haque <famiuhaque@gmail.com>
2021-04-19 10:31:48 +0600

    Use Git blame instead of deprecated Gblame

--- lua/config/tools/fugitive_gv.lua
+++ lua/config/tools/fugitive_gv.lua
@@ -1,6 +1,6 @@
 local bind = vim.api.nvim_set_keymap
 
-bind('n', '<Space>gb', ':Gblame<CR>', {})
+bind('n', '<Space>gb', ':Git blame<CR>', {})
 bind('n', '<Space>gs', ':Git<CR>', {})
 bind('n', '<Space>gc', ':Git commit -v<CR>', {})
 bind('n', '<Space>ga', ':Git add -p<CR>', {})
