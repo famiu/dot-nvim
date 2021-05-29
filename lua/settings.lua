c46ab0d0db7fae679d66cd29420fa2a98d030b20
Famiu Haque <famiuhaque@gmail.com>
2021-05-29 17:55:27 +0600

    Fix guicursor option with vim.opt

--- lua/settings.lua
+++ lua/settings.lua
@@ -34,9 +34,12 @@ opt.secure = true
 
 -- GUI cursor
 opt.guicursor = {
-    ['n-v-c'] = 'block', ['i-ci-ve'] = 'ver25', ['r-cr'] = 'hor20',
-    o = 'hor50', a = 'blinkwait700-blinkoff400-blinkon250-Cursor/lCursor',
-    sm = 'block-blinkwait175-blinkoff150-blinkon175'
+    'n-v-c:block',
+    'i-ci-ve:ver25',
+    'r-cr:hor20',
+    'o:hor50',
+    'a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor',
+    'sm:block-blinkwait175-blinkoff150-blinkon175'
 }
 
 -- Persistent undo
