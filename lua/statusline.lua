4e5f9227e80990ec7e6cf5c37790e523d26460fd
Famiu Haque <famiuhaque@protonmail.com>
2021-09-03 12:51:25 +0600

    Fix inactive statusline highlight

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -8,19 +8,18 @@ components.left.inactive = {}
 components.mid.inactive = {}
 components.right.inactive = {}
 
--- Get highlight of inactive statusline by parsing the style, fg and bg of VertSplit
 local InactiveStatusHL = {
-    fg = exec("highlight VertSplit", true):match("guifg=(#%d+)") or '#444444',
-    bg = exec("highlight VertSplit", true):match("guibg=(#%d+)") or '#1E1E1E',
-    style = exec("highlight VertSplit", true):match("gui=(#%d+)") or ''
+    fg = exec("highlight VertSplit", true):match("guifg=(#[0-9A-Fa-f]+)") or "#444444",
+    bg = exec("highlight VertSplit", true):match("guibg=(#[0-9A-Fa-f]+)") or "#1E1E1E",
+    style = exec("highlight VertSplit", true):match("gui=(#[0-9A-Fa-f]+)") or "",
 }
 
 -- Add strikethrough to inactive statusline highlight style
 -- in order to have a thin line instead of the statusline
 if InactiveStatusHL.style == '' then
-    InactiveStatusHL.style = 'strikethrough'
+    InactiveStatusHL.style = 'underline'
 else
-    InactiveStatusHL.style = InactiveStatusHL.style .. ',strikethrough'
+    InactiveStatusHL.style = InactiveStatusHL.style .. ',underline'
 end
 
 -- Apply the highlight to the statusline
