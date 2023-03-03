f87951377d11822b37c2c2574fd32329a156fd20
Famiu Haque <famiuhaque@proton.me>
2023-03-03 16:05:54 +0600

    fix autocd

--- lua/utilities/autocd.lua
+++ lua/utilities/autocd.lua
@@ -1,5 +1,6 @@
 local api = vim.api
 local lsp = vim.lsp
+local fs = vim.fs
 
 -- Automatically change to project root directory using either LSP or configured root patterns
 local root_patterns = { '.git', 'Makefile', 'CMakeLists.txt' }
@@ -9,27 +10,21 @@ local function autocd()
 
     for _, client in ipairs(clients) do
         if client.config.root_dir then
-            vim.fn.chdir(client.config.root_dir)
+            pcall(api.nvim_set_current_dir, client.config.root_dir)
             return
         end
     end
 
     local bufname = api.nvim_buf_get_name(0)
-    -- Parse URLs such as file:// and fugitive:// and extract the file path from buffer name
-    local path_match = bufname:match([[^([a-zA-Z]+:/)(/.*)]])
+    local path = fs.dirname(bufname)
 
-    if path_match == nil or path_match[2] == nil then
-        return
-    end
-
-    local path = vim.fs.dirname(path_match[2])
-    local root_pattern_match = vim.fs.find(root_patterns, { path = path, upward = true })[1]
+    local root_pattern_match = fs.find(root_patterns, { path = path, upward = true })[1]
 
     if root_pattern_match == nil then
         return
     end
 
-    vim.fn.chdir(vim.fs.dirname(root_pattern_match))
+    pcall(api.nvim_set_current_dir, fs.dirname(root_pattern_match))
 end
 
 local augroup = api.nvim_create_augroup('AutoCD', {})
