0290e97f450394eb9d3debf9cd845e0b09c2258c
Famiu Haque <famiuhaque@proton.me>
2023-03-03 16:06:04 +0600

    change git mappings

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -6,17 +6,11 @@ return {
         init = function()
             local keymap = vim.keymap
 
-            keymap.set('n', '<Space>gb', ':Git blame<CR>', {})
-            keymap.set('n', '<Space>gs', ':Git<CR>', {})
-            keymap.set('n', '<Space>gc', ':Git commit -v<CR>', {})
-            keymap.set('n', '<Space>ga', ':Git add -p<CR>', {})
-            keymap.set('n', '<Space>gm', ':Git commit --amend<CR>', {})
-            keymap.set('n', '<Space>gp', ':Git push<CR>', {})
-            keymap.set('n', '<Space>gf', ':Git fetch<CR>', {})
-            keymap.set('n', '<Space>gF', ':Git pull<CR>', {})
-            keymap.set('n', '<Space>gR', ':Git pull --rebase<CR>', {})
-            keymap.set('n', '<Space>gd', ':Gdiff<CR>', {})
-            keymap.set('n', '<Space>gw', ':Gwrite<CR>', {})
+            keymap.set('n', '<Leader>gb', ':Git blame<CR>', {})
+            keymap.set('n', '<Leader>gs', ':silent! Git<CR>', {})
+            keymap.set('n', '<Leader>ga', ':Git add -p<CR>', {})
+            keymap.set('n', '<Leader>gc', ':Git commit -v<CR>', {})
+            keymap.set('n', '<Leader>gm', ':Git commit --amend<CR>', {})
         end
     },
     {
