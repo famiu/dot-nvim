a07455a78e4270120938c0231be7c1566abc287a
Famiu Haque <famiuhaque@proton.me>
2023-06-01 15:15:28 +0600

    fix dap config

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,16 +3,7 @@ local fn = vim.fn
 local keymap = vim.keymap
 local augroup = api.nvim_create_augroup('dap-settings', {})
 
-local M = {
-    'mfussenegger/nvim-dap',
-    lazy = true,
-    dependencies = {
-        'rcarriga/nvim-dap-ui',
-        'theHamsta/nvim-dap-virtual-text'
-    }
-}
-
-function M.init()
+local function dapinit()
     -- Keybindings
     keymap.set('n', '<F5>', function() require('dap').continue() end)
     keymap.set('n', '<F6>', function() require('dap').step_back() end)
@@ -21,18 +12,18 @@ function M.init()
     keymap.set('n', '<F12>', function() require('dap').step_out() end)
     keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
     keymap.set(
-        'n', '<Leader>B',
-        function() require('dap').set_breakpoint(fn.input('Breakpoint condition: ')) end
+    'n', '<Leader>B',
+    function() require('dap').set_breakpoint(fn.input('Breakpoint condition: ')) end
     )
     keymap.set(
-        'n', '<Leader>lp',
-        function() require('dap').set_breakpoint(nil, nil, fn.input('Log point message: ')) end
+    'n', '<Leader>lp',
+    function() require('dap').set_breakpoint(nil, nil, fn.input('Log point message: ')) end
     )
     keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
     keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
 end
 
-function M.config()
+local function dapconfig()
     local dap = require('dap')
     local dapui = require('dapui')
 
@@ -51,6 +42,9 @@ function M.config()
         group = augroup
     })
 
+    -- DAP UI
+    dapui.setup()
+
     dap.listeners.after.event_initialized['dapui_config'] = function()
         dapui.open()
     end
@@ -66,12 +60,11 @@ function M.config()
 
     dap.adapters.lldb = {
         type = 'executable',
-        command = vim.trim(fn.system({ '/usr/bin/which', 'lldb-vscode ' })),
+        command = vim.trim(fn.system({ '/usr/bin/which', 'lldb-vscode' })),
         name = 'lldb'
     }
 
-    -- Language configurations
-    dap.configurations.cpp = {
+    local base_config = {
         {
             name = 'Launch',
             type = 'lldb',
@@ -93,8 +86,25 @@ function M.config()
         },
     }
 
-    dap.configurations.c = dap.configurations.cpp
-    dap.configurations.rust = dap.configurations.cpp
+    -- Language configurations
+    dap.configurations.cpp = base_config
+    dap.configurations.c = base_config
+    dap.configurations.rust = base_config
+
+    -- Program specific configs
+    table.insert(dap.configurations.c, {
+        name = 'Neovim',
+        type = 'lldb',
+        request = 'launch',
+        program = '/home/famiu/Workspace/neovim/neovim/build/bin/nvim',
+        externalConsole = true,
+    })
 end
+
+local M = {
+    { 'mfussenegger/nvim-dap', lazy = true, init = dapinit, config = dapconfig },
+    { 'rcarriga/nvim-dap-ui', lazy = true, dependencies = 'mfussenegger/nvim-dap' },
+    { 'theHamsta/nvim-dap-virtual-text', lazy = true, dependencies = 'mfussenegger/nvim-dap' },
+}
 
 return M
