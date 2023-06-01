578fc070a63222389b0cbef4bce535caff25c83b
Famiu Haque <famiuhaque@proton.me>
2023-06-01 17:48:11 +0600

    configure dap for debugging Neovim

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -100,16 +100,74 @@ local function dapconfig()
     dap.configurations.rust = c_cpp_rust_base_config
 
     -- Program specific configs
-    table.insert(dap.configurations.c, {
-        name = 'Neovim',
-        type = 'lldb',
-        request = 'launch',
-        program = '/home/famiu/Workspace/neovim/neovim/build/bin/nvim',
-        cwd = '${workspaceFolder}',
-        stopOnEntry = false,
-        args = { '--clean' },
-        runInTerminal = true,
-    })
+    table.insert(dap.configurations.c, setmetatable(
+        {
+            name = 'Neovim',
+            type = 'lldb',
+            request = 'launch',
+            program = os.getenv('HOME') .. '/Workspace/neovim/neovim/build/bin/nvim',
+            cwd = '${workspaceFolder}',
+            stopOnEntry = false,
+            args = { '--clean' },
+            runInTerminal = true,
+        },
+        {
+            __call = function(config)
+                -- Listeners are indexed by a key.
+                -- This is like a namespace and must not conflict with what plugins
+                -- like nvim-dap-ui or nvim-dap itself uses.
+                -- It's best to not use anything starting with `dap`
+                local key = 'neovim-debug-auto-attach'
+
+                -- dap.listeners.<before | after>.<event_or_command>.<plugin_key>`
+                -- We listen to the `initialize` response. It indicates a new session got initialized
+                dap.listeners.after.initialize[key] = function(session)
+                    -- Immediately clear the listener, we don't want to run this logic for additional sessions
+                    dap.listeners.after.initialize[key] = nil
+
+                    -- The first argument to a event or response is always the session
+                    -- A session contains a `on_close` table that allows us to register functions
+                    -- that get called when the session closes.
+                    -- We use this to ensure the listeners get cleaned up
+                    session.on_close[key] = function()
+                        for _, handler in pairs(dap.listeners.after) do
+                            handler[key] = nil
+                        end
+                    end
+                end
+
+                -- We listen to `event_process` to get the pid:
+                dap.listeners.after.event_process[key] = function(_, body)
+                    -- Immediately clear the listener, we don't want to run this logic for additional sessions
+                    dap.listeners.after.event_process[key] = nil
+
+                    local ppid = body.systemProcessId
+                    -- The pid is the parent pid, we need to attach to the child. This uses the `ps` tool to get it
+                    -- It takes a bit for the child to arrive. This uses the `vim.wait` function to wait up to a second
+                    -- to get the child pid.
+                    vim.wait(1000, function()
+                        return tonumber(vim.fn.system('ps -o pid= --ppid ' .. tostring(ppid))) ~= nil
+                    end)
+                    local pid = tonumber(vim.fn.system('ps -o pid= --ppid ' .. tostring(ppid)))
+
+                    -- If we found it, spawn another debug session that attaches to the pid.
+                    if pid then
+                        dap.run({
+                            name = 'Neovim embedded',
+                            type = 'lldb',
+                            request = 'attach',
+                            pid = pid,
+                            -- ⬇️ Change paths as needed
+                            program = os.getenv('HOME') .. '/Workspace/neovim/neovim/build/bin/nvim',
+                            cwd = os.getenv('HOME') .. '/Workspace/neovim/neovim/',
+                            externalConsole = false,
+                        })
+                    end
+                end
+                return config
+            end
+        }
+    ))
 end
 
 local M = {
