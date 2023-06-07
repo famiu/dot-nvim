a9f5a1add2fee204be8bfc486a9e88bc50fa3318
Famiu Haque <famiuhaque@proton.me>
2023-06-07 13:25:23 +0600

    use flit.nvim

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -8,4 +8,11 @@ return {
         dependencies = 'ggandor/leap.nvim',
         config = true
     },
+    {
+        'ggandor/flit.nvim',
+        dependencies = { 'ggandor/leap.nvim', 'tpope/vim-repeat' },
+        opts = {
+            labeled_modes = "nvo",
+        },
+    },
 }
