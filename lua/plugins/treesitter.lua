218ac6060fb4fedc8619368a6739ed87f9a3801e
Famiu Haque <famiuhaque@proton.me>
2023-06-08 00:20:39 +0600

    make sure treesitter-textobjects doesn't conflict with flit.nvim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -120,17 +120,6 @@ function M.config()
             },
         },
     }
-
-    local ts_repeat_move = require('nvim-treesitter.textobjects.repeatable_move')
-
-    -- Repeat movement with ; and ,
-    -- Ensure ; goes forward and , goes backward regardless of the last direction
-    vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move_next)
-    vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_previous)
-    vim.keymap.set({ 'n', 'x', 'o' }, 'f', ts_repeat_move.builtin_f)
-    vim.keymap.set({ 'n', 'x', 'o' }, 'F', ts_repeat_move.builtin_F)
-    vim.keymap.set({ 'n', 'x', 'o' }, 't', ts_repeat_move.builtin_t)
-    vim.keymap.set({ 'n', 'x', 'o' }, 'T', ts_repeat_move.builtin_T)
 end
 
  return M
