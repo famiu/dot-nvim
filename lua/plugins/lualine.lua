a860aafd734ade6b1ef85b1a3925d68331e460c8
Famiu Haque <famiuhaque@proton.me>
2023-06-08 19:34:47 +0600

    configure lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,23 +3,62 @@ return {
     dependencies = {
         'kyazdani42/nvim-web-devicons',
         'SmiteshP/nvim-navic',
-        'EdenEast/nightfox.nvim'  -- Colorscheme is needed for Lualine theme
+        'EdenEast/nightfox.nvim', -- Colorscheme is needed for Lualine theme
+        'folke/noice.nvim',       -- For mode
     },
-    opts = {
-        winbar = {
-            lualine_c = { 'filename' },
-            lualine_x = {
-                function()
-                    if require('nvim-navic').is_available() then
-                        return require('nvim-navic').get_location()
-                    else
-                        return ''
+    config = function()
+        local noice = require('noice')
+        local navic = require('nvim-navic')
+
+        require('lualine').setup {
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = { 'filename' },
+                lualine_x = {
+                    {
+                        noice.api.status.mode.get,
+                        cond = noice.api.status.mode.has,
+                        color = { fg = '#ff9e64' },
+                    },
+                    'encoding',
+                    'fileformat',
+                    'filetype'
+                },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' }
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { 'filename' },
+                lualine_x = { 'location' },
+                lualine_y = {},
+                lualine_z = {}
+            },
+            winbar = {
+                lualine_c = { { 'filename', path = 1 } },
+                lualine_x = {
+                    function()
+                        if navic.is_available() then
+                            return navic.get_location()
+                        else
+                            return ''
+                        end
                     end
-                end
+                }
+            },
+            inactive_winbar = {
+                lualine_c = { { 'filename', path = 1 } },
+            },
+            extensions = {
+                'man',
+                'quickfix',
+                'fugitive',
+                'lazy',
+                'nvim-dap-ui',
+                'toggleterm',
             }
-        },
-        inactive_winbar = {
-            lualine_c = { 'filename' },
         }
-    }
+    end
 }
