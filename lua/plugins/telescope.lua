900c1a54b505a5f8dba09fa43e23017ac5ca3aeb
Famiu Haque <famiuhaque@proton.me>
2023-06-16 20:41:03 +0600

    remove unused keybinding for telescope live grep args

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -48,7 +48,6 @@ return {
                         mappings = {
                             i = {
                                 ['<C-k>'] = lga_actions.quote_prompt(),
-                                ['<C-i>'] = lga_actions.quote_prompt({ postfix = ' --iglob ' }),
                             },
                         },
                     },
