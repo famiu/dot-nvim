354b838cc75f0cf4944632eb9433de0692d8bb60
Famiu Haque <famiuhaque@proton.me>
2023-11-30 19:46:02 +0600

    add luasnip latex snippets

--- lua/plugins/language/latex.lua
+++ lua/plugins/language/latex.lua
@@ -19,6 +19,16 @@ return {
                 },
             }
         end,
-        'micangl/cmp-vimtex',
+        { 'micangl/cmp-vimtex', dependencies = { 'lervag/vimtex', 'hrsh7th/nvim-cmp' } },
+        {
+            'iurimateus/luasnip-latex-snippets.nvim',
+            -- vimtex isn't required if using treesitter
+            dependencies = { 'L3MON4D3/LuaSnip', 'lervag/vimtex' },
+            opts = {},
+            config = function(_, opts)
+                require('luasnip-latex-snippets').setup(opts)
+                require('luasnip').config.setup({ enable_autosnippets = true })
+            end,
+        },
     },
 }
