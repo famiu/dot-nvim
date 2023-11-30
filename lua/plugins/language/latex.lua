f752e4a2c6d2d4204178c455e994a1c0df45e245
Famiu Haque <famiuhaque@proton.me>
2023-11-30 18:59:36 +0600

    modularize language plugin specs

---
+++ lua/plugins/language/latex.lua
@@ -0,0 +1,24 @@
+return {
+    {
+        'lervag/vimtex',
+        init = function()
+            vim.g.vimtex_view_method = 'zathura_simple'
+            vim.g.vimtex_view_forward_search_on_start = 0
+            vim.g.vimtex_compiler_latexmk = {
+                aux_dir = './aux/',
+                out_dir = '',
+                callback = true,
+                continuous = true,
+                executable = 'latexmk',
+                hooks = {},
+                options = {
+                    '-verbose',
+                    '-file-line-error',
+                    '-synctex=1',
+                    '-interaction=nonstopmode',
+                },
+            }
+        end,
+        'micangl/cmp-vimtex',
+    },
+}
