40828fb4573ca53d24cd651d569af320c11759c7
Famiu Haque <famiuhaque@proton.me>
2024-03-21 00:43:04 +0600

    formatting

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -27,5 +27,5 @@ return {
         -- Make LSP floating windows have borders.
         lsp.handlers['textDocument/hover'] = lsp.with(lsp.handlers.hover, { border = 'single' })
         lsp.handlers['textDocument/signatureHelp'] = lsp.with(lsp.handlers.signature_help, { border = 'single' })
-    end
+    end,
 }
