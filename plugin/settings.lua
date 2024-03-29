1025fcbf4596ef4de2fed6f76518fd52f41d3328
Famiu Haque <famiuhaque@proton.me>
2024-03-29 09:59:51 +0600

    revamp option configuration

--- plugin/settings.lua
+++ plugin/settings.lua
@@ -1,14 +1,17 @@
 -- Use ripgrep for grepprg
 vim.o.grepprg = 'rg --vimgrep --smart-case'
 
---- Wildmenu
-vim.o.wildmode = 'longest,list,full'
+-- Better command line completion
+vim.o.wildmode = 'longest:list,full'
 
--- Persistent undo
+-- Persistent undo, prevent Neovim from having Alzheimer's
 vim.o.undofile = true
 
--- Use global statusline
+-- Use global statusline, not because I made it or anything
 vim.o.laststatus = 3
+
+-- Allow virtual editing in visual block mode and after the end of the line
+vim.o.virtualedit = 'block,onemore'
 
 -- Spaces > Tabs
 vim.o.softtabstop = 4
@@ -21,7 +24,7 @@ vim.o.linebreak = true
 vim.o.breakindent = true
 vim.o.showbreak = '↪ '
 
--- Fill column
+-- Fill column indicator
 vim.o.colorcolumn = '+1'
 
 -- Use hybrid line numbers
@@ -34,7 +37,7 @@ vim.o.inccommand = 'split'
 -- Use space for diff fillchars
 vim.o.fillchars = 'diff: '
 
--- Fold settings
+-- Use transparent fold and disable folding by default
 vim.o.foldtext = ''
 vim.o.foldenable = false
 vim.o.foldminlines = 4
@@ -42,14 +45,14 @@ vim.o.foldminlines = 4
 -- Session options
 vim.o.sessionoptions = 'blank,folds,help,tabpages,winsize,winpos,terminal,localoptions'
 
--- Search
+-- Use smartcase for searching
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Make substitute global by default
 vim.o.gdefault = true
 
--- Completion
+-- Settings for insert mode completion
 vim.o.completeopt = 'menuone,preview,noselect'
 vim.o.shortmess = vim.o.shortmess .. 'c'
 
@@ -57,8 +60,8 @@ vim.o.shortmess = vim.o.shortmess .. 'c'
 vim.o.splitbelow = true
 vim.o.splitright = true
 
--- Diff options
-vim.o.diffopt = 'internal,filler,closeoff,hiddenoff,vertical,iwhite,indent-heuristic,linematch:60'
+-- Better diff behavior
+vim.o.diffopt = 'internal,filler,closeoff,hiddenoff,vertical,indent-heuristic,linematch:60'
 
 -- Faster update time
 vim.o.updatetime = 100
@@ -66,25 +69,27 @@ vim.o.updatetime = 100
 -- Highlight current line
 vim.o.cursorline = true
 
--- Scroll
+-- Scroll offsets
 vim.o.scrolloff = 10
 vim.o.sidescrolloff = 5
+
+-- Scroll based on screen lines instead of logical lines
 vim.o.smoothscroll = true
 
 -- Allow project specific configuration
 vim.o.exrc = true
 
--- Mouse
-vim.o.mouse = 'nv'
-vim.o.mousemoveevent = true
-
--- Statuscolumn
+-- Statuscolumn, show (hybrid) line number before signcolumn
 vim.o.signcolumn = 'auto:2'
 vim.o.statuscolumn = "%{%v:relnum?'%='.v:relnum:v:lnum.'%='%} %s"
 
--- Conceal
+-- Allow conceal to use replacement characters to hide text
 vim.o.conceallevel = 2
 
+-- Better listchars
+vim.o.list = true
+vim.o.listchars='tab:» ,extends:›,precedes:‹,nbsp:␣'
+
 -- Remove "How-to disable mouse" from right-click menu
-vim.cmd.aunmenu([[PopUp.How-to\ disable\ mouse]])
-vim.cmd.aunmenu([[PopUp.-1-]])
+pcall(vim.cmd.aunmenu, [[PopUp.How-to\ disable\ mouse]])
+pcall(vim.cmd.aunmenu, [[PopUp.-1-]])
