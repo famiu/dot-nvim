1b9f63cb31f215de65ab66ee3e36e4ed6ae898ad
Famiu Haque <famiuhaque@proton.me>
2024-03-29 08:19:39 +0600

    Refactor AutoCD utility

--- plugin/autocd.lua
+++ plugin/autocd.lua
@@ -1,39 +1,40 @@
 local api = vim.api
-local fs = vim.fs
 
--- Automatically change to project root directory using either LSP or configured root patterns
-local root_patterns = { '.git', 'Makefile', 'CMakeLists.txt' }
+local root_patterns = { '.git' }
 
+-- Automatically change to project root directory using configured root patterns.
 local function autocd()
-    local clients = vim.lsp.get_clients({ bufnr = 0 })
-    local target_dir
+    local bufname = api.nvim_buf_get_name(0)
+    local path = vim.fs.dirname(bufname)
 
-    for _, client in ipairs(clients) do
-        -- Ignore copilot as it doesn't have a valid root directory.
-        if client.name ~= 'copilot' and client.config.root_dir then
-            target_dir = client.config.root_dir
-            break
-        end
+    local root_pattern_match = vim.fs.find(root_patterns, { path = path, stop = vim.uv.os_homedir(), upward = true })[1]
+
+    if root_pattern_match == nil then
+        return
     end
 
-    if target_dir == nil then
-        local bufname = api.nvim_buf_get_name(0)
-        local path = fs.dirname(bufname)
+    vim.cmd.lcd(vim.fs.dirname(root_pattern_match))
+end
 
-        local root_pattern_match = fs.find(root_patterns, { path = path, upward = true })[1]
+-- Automatically change current directory to LSP root directory on LSP attach.
+local function lsp_autocd(args)
+    -- Ignore copilot as it doesn't have a valid root directory.
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    assert(client ~= nil)
 
-        if root_pattern_match == nil then
-            return
-        end
-
-        target_dir = fs.dirname(root_pattern_match)
+    if client.name ~= 'copilot' and client.config.root_dir then
+        vim.cmd.lcd(client.config.root_dir)
     end
-
-    pcall(api.nvim_set_current_dir, target_dir)
 end
 
 local augroup = api.nvim_create_augroup('AutoCD', {})
+
 api.nvim_create_autocmd(
-    { 'VimEnter', 'BufEnter', 'BufReadPost', 'LspAttach' },
-    { group = augroup, callback = autocd, desc = 'Automatically change current directory' }
+    { 'VimEnter', 'BufEnter', 'BufReadPost' },
+    { group = augroup, callback = autocd, desc = 'Automatically change current directory by matching root pattern' }
+)
+
+api.nvim_create_autocmd(
+    { 'LspAttach' },
+    { group = augroup, callback = lsp_autocd, desc = 'Automatically change current directory to LSP root' }
 )
