615c32cf6450fa8c3161dc059869974d744ba4ea
Famiu Haque <famiuhaque@proton.me>
2024-04-02 05:57:41 +0600

    reorder nvim-cmp comparators

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -157,12 +157,10 @@ return {
                 sorting = {
                     priority_weight = 2,
                     comparators = {
-                        require('copilot_cmp.comparators').prioritize,
-
-                        -- Below is the default comparitor list and order for nvim-cmp
                         cmp.config.compare.offset,
-                        -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
                         cmp.config.compare.exact,
+                        require('copilot_cmp.comparators').prioritize,
+                        -- cmp.config.compare.scopes,
                         cmp.config.compare.score,
                         cmp.config.compare.recently_used,
                         cmp.config.compare.locality,
