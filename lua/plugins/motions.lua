33aaf12bccc3d11f1b04d42ba401693e9a085536
Famiu Haque <famiuhaque@proton.me>
2024-04-01 12:00:05 +0600

    fix flash.nvim conflict with qf and cmdwin

--- lua/plugins/motions.lua
+++ lua/plugins/motions.lua
@@ -50,6 +50,26 @@ return {
             { 't', mode = { 'n', 'x', 'o' } },
             { 'T', mode = { 'n', 'x', 'o' } },
         },
+        init = function()
+            -- Unmap <CR> in quickfix and command-line windows
+            local flash_unmap_augroup = vim.api.nvim_create_augroup('FlashUnmapCR', {})
+            local flash_unmap_fn = function()
+                vim.keymap.set('n', '<CR>', '<CR>', { buffer = 0 })
+            end
+
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = 'qf',
+                callback = flash_unmap_fn,
+                group = flash_unmap_augroup,
+                desc = 'Unmap <CR> for quickfix windows',
+            })
+
+            vim.api.nvim_create_autocmd('CmdwinEnter', {
+                callback = flash_unmap_fn,
+                group = flash_unmap_augroup,
+                desc = 'Unmap <CR> for command-line windows',
+            })
+        end,
         config = function(_, opts)
             require('flash').setup(opts)
 
@@ -61,6 +81,8 @@ return {
                         require('flash').toggle(false)
                     end
                 end,
+                group = vim.api.nvim_create_augroup('FlashCmdlineToggle', {}),
+                desc = 'Toggle Flash search jump labels off when entering cmdline',
             })
         end,
     },
