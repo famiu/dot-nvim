5760ae020359cd7c29d53d4c40b5a945b0f375c1
Famiu Haque <famiuhaque@proton.me>
2024-04-08 22:03:41 +0600

    refactor luals configuration and update plugins

--- lua/plugins/lsp/clients.lua
+++ lua/plugins/lsp/clients.lua
@@ -2,47 +2,46 @@ return {
     lua_ls = {
         cmd = { 'lua-language-server' },
         settings = {
-            Lua = {
-                workspace = {
-                    checkThirdParty = false,
-                },
-            },
+            Lua = {},
         },
         on_init = function(client)
-            local nvim_runtime_dirs = vim.api.nvim_get_runtime_file('', true)
-            local is_nvim_lua = false
-
-            if require('utilities.os').is_windows() then
-                -- Use forward slashes for Windows paths in runtime_dirs since root_dir uses forward slashes.
-                for i, dir in ipairs(nvim_runtime_dirs) do
-                    nvim_runtime_dirs[i] = dir:gsub('\\', '/')
-                end
+            if client.config.root_dir == nil then
+                return
             end
 
-            -- Check if the root_dir is inside a Neovim runtime directory.
+            local path = vim.fs.normalize(client.config.root_dir)
+
+            local nvim_runtime_dirs = vim.tbl_map(
+                function(dir) return vim.fs.normalize(dir) end,
+                vim.api.nvim_list_runtime_paths()
+            )
+
+            local in_nvim_runtime = false
+
             for _, dir in ipairs(nvim_runtime_dirs) do
-                if vim.startswith(client.config.root_dir, dir) then
-                    is_nvim_lua = true
+                if vim.startswith(path, dir) then
+                    in_nvim_runtime = true
                     break
                 end
             end
 
-            if not is_nvim_lua then
+            if not in_nvim_runtime then
                 return
             end
 
-            local workspace_libraries = nvim_runtime_dirs
-            -- Add busted and luv to the workspace library.
-            table.insert(workspace_libraries, '${3rd}/luv/library')
-            table.insert(workspace_libraries, '${3rd}/busted/library')
-
             client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
                 runtime = {
+                    -- Use LuaJIT for Neovim.
                     version = 'LuaJIT',
                 },
                 -- Make the server aware of Neovim runtime files.
                 workspace = {
-                    library = workspace_libraries,
+                    checkThirdParty = false,
+                    library = {
+                        '${3rd}/luv/library',
+                        '${3rd}/busted/library',
+                        unpack(nvim_runtime_dirs),
+                    },
                 },
             })
         end,
