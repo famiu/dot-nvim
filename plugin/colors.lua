45222ace13b822bba909ab473d305cf0c01a2dd5
Famiu Haque <famiuhaque@proton.me>
2024-06-30 08:11:42 +0600

    customize colorscheme a bit

--- plugin/colors.lua
+++ plugin/colors.lua
@@ -1,4 +1,8 @@
-vim.api.nvim_set_hl(0, 'Keyword', {
-    fg = 'NvimLightMagenta',
-    force = true,
-})
+-- Custom overrides for default colorscheme
+
+vim.api.nvim_set_hl(0, 'Keyword', { fg = 'NvimLightRed' })
+vim.api.nvim_set_hl(0, 'Identifier', { fg = '#E6CDD3' })
+vim.api.nvim_set_hl(0, 'Function', { fg = 'NvimLightBlue' })
+vim.api.nvim_set_hl(0, 'Type', { fg = 'NvimLightYellow' })
+vim.api.nvim_set_hl(0, 'Special', { fg = 'NvimLightCyan' })
+vim.api.nvim_set_hl(0, 'Constant', { fg = '#C8DACD' })
