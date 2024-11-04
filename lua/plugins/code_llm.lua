7054b46a13b01675ebb274d905b4648f286ec0b8
Famiu Haque <famiuhaque@proton.me>
2024-11-05 05:59:52 +0600

    re-add copilot and avante

---
+++ lua/plugins/code_llm.lua
@@ -0,0 +1,41 @@
+return {
+    {
+        'zbirenbaum/copilot.lua',
+        cmd = 'Copilot',
+        event = 'InsertEnter',
+        opts = {
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                hide_during_completion = true,
+                debounce = 75,
+                keymap = {
+                    accept = '<M-l>',
+                    accept_word = '<C-Right>',
+                    accept_line = '<S-Right>',
+                    next = '<M-]>',
+                    prev = '<M-[>',
+                    dismiss = '<C-]>',
+                },
+            },
+            server_opts_overrides = {},
+        },
+    },
+    {
+        'yetone/avante.nvim',
+        opts = {
+            provider = 'copilot',
+        },
+        build = require('utilities.os').is_windows()
+                and 'powershell -ExecutionPolicy Bypass -File Build -BuildFromSource false'
+            or 'make',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+            'stevearc/dressing.nvim',
+            'nvim-lua/plenary.nvim',
+            'MunifTanjim/nui.nvim',
+            'nvim-tree/nvim-web-devicons',
+            'zbirenbaum/copilot.lua',
+        },
+    },
+}
