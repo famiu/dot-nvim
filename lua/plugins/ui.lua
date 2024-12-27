4e3e44421bb5976d3c81c77be4a0a152fc12fc34
Famiu Haque <famiuhaque@proton.me>
2024-12-27 15:59:40 +0600

    add image.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,5 +1,25 @@
 return {
     {
+        '3rd/image.nvim',
+        build = false,
+        opts = {
+            backend = 'kitty',
+            processor = 'magick_cli',
+            integrations = {
+                markdown = {
+                    enabled = true,
+                    clear_in_insert_mode = false,
+                    download_remote_images = true,
+                    only_render_image_at_cursor = false,
+                    floating_windows = false, -- if true, images will be rendered in floating markdown windows
+                    filetypes = { 'markdown', 'vimwiki' }, -- markdown extensions (ie. quarto) can go here
+                },
+                html = { enabled = false },
+                css = { enabled = false },
+            },
+        },
+    },
+    {
         'luukvbaal/statuscol.nvim',
         config = function()
             local builtin = require('statuscol.builtin')
