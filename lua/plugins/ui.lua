de6eed5fd58e139333a6093a12172306a57d56cb
Famiu Haque <famiuhaque@proton.me>
2025-01-06 02:53:09 +0600

    add notify background color

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -88,6 +88,10 @@ return {
     {
         'rcarriga/nvim-notify',
         config = function()
+            require('notify').setup({
+                background_colour = '#000000',
+            })
+
             vim.notify = require('notify')
         end,
     },
