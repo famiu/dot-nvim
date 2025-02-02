a9791fe505627d94bdcba64a77be86ebf1cc711e
Famiu Haque <famiuhaque@proton.me>
2025-02-02 12:11:35 +0600

    update plugins

--- lua/plugins/language/markdown.lua
+++ lua/plugins/language/markdown.lua
@@ -5,15 +5,5 @@ return {
         'nvim-treesitter/nvim-treesitter',
         'nvim-tree/nvim-web-devicons',
     },
-    opts = {
-        modes = { 'n', 'no', 'c' },
-        hybrid_modes = { 'n' },
-
-        callbacks = {
-            on_enable = function(_, win)
-                vim.wo[win].conceallevel = 2
-                vim.wo[win].concealcursor = 'c'
-            end,
-        },
-    },
+    opts = {},
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -8,7 +8,7 @@ return {
             integrations = {
                 markdown = {
                     enabled = true,
-                    clear_in_insert_mode = false,
+                    clear_in_insert_mode = true,
                     download_remote_images = true,
                     only_render_image_at_cursor = false,
                     floating_windows = false, -- if true, images will be rendered in floating markdown windows
