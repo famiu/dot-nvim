f2d39fe68296744977d89e85b73c7bffe8383cdb
Famiu Haque <famiuhaque@proton.me>
2025-05-07 17:09:34 +0600

    enable gitsigns current line blame

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -102,14 +102,16 @@ return {
                 follow_files = true,
             },
             attach_to_untracked = true,
-            current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+            current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
             current_line_blame_opts = {
                 virt_text = true,
                 virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-                delay = 1000,
+                delay = 100,
                 ignore_whitespace = false,
+                virt_text_priority = 100,
+                use_focus = true,
             },
-            current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+            current_line_blame_formatter = '  <author>, <author_time:%R> - <summary>',
             sign_priority = 6,
             update_debounce = 100,
             status_formatter = nil, -- Use default
