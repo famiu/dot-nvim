2d6c1379050737cfedc5f3df29acc210f273de0e
Famiu Haque <famiu@famiu.dev>
2026-06-24 01:08:24 +0600

    replace markview.nvim with render-markdown.nvim

--- lua/plugins/language/markdown.lua
+++ lua/plugins/language/markdown.lua
@@ -1,9 +1,11 @@
 return {
-    'OXY2DEV/markview.nvim',
+    'MeanderingProgrammer/render-markdown.nvim',
     ft = 'markdown',
     dependencies = {
         'nvim-treesitter/nvim-treesitter',
         'nvim-tree/nvim-web-devicons',
     },
+    --- @module 'render-markdown'
+    --- @type render.md.UserConfig
     opts = {},
 }
